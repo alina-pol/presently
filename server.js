e51b0ea4279b5b09d2dfedfdbdce83832d53ce27
Alina232323 <alina.polianska@vergesense.com>
2023-04-08 13:55:01 -0700

    added mongoose config

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
+mongoose.set('strictQuery', false)
 require('dotenv').config()
 const appController = require('./controllers/meditations.js')
 const usersController = require('./controllers/users.js')
